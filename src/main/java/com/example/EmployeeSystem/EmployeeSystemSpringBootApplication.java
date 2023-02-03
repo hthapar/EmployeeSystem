4c29cddf40107aa91a84863bbbb3544996bc014e
Himanshu Thapar <himanshu.thapar@springernature.com>
2023-02-03 11:06:58 +0530

    Setup Employee System Project

---
+++ src/main/java/com/example/EmployeeSystem/EmployeeSystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSystemSpringBootApplication.class, args);
+	}
+
+}
